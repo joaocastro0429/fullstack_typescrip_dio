21fbd84dbe73ac48403018f61a3ddd0f2a884f9c
joaocastro <joaocastro0429@gmail.com>
2024-08-24 10:29:04 -0300

    add rotas

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
-console.log("hello word")
+import express, { response } from 'express'
+
+const server=express()
+
+server.use(express.json())
+
+server.get("/users",(request, response)=>{
+    return response.json({message:"dioBlanck API"})
+})
+
+server.post("/user",(request,response)=>{
+    const body=request.body
+    console.log(body)
+    return response.status(201).json({message:"usuário criado"})
+})
+
+
+server.listen(5555,()=>console.log('running run server'))
